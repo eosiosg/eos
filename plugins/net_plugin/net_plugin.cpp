8298684233be45c84222d448b639ca228dabb8c6
oldcold <jinliang.liu@hotmail.com>
2019-01-09 00:03:14 +0800

    add debug log;

--- plugins/net_plugin/net_plugin.cpp
+++ plugins/net_plugin/net_plugin.cpp
@@ -2855,6 +2855,7 @@ namespace eosio {
     void net_plugin_impl::pbft_outgoing_prepare(const pbft_prepare &msg) {
         auto added = maybe_add_pbft_cache(msg.uuid);
         if (!added) return;
+        ilog("sending prepare at height: ${h}, view: ${v}", ("h", msg.block_num)("v", msg.view));
 
         pbft_controller &pcc = my_impl->chain_plug->pbft_ctrl();
         if (!pcc.pbft_db.is_valid_prepare(msg)) return;
@@ -2932,7 +2933,7 @@ namespace eosio {
 
        auto added = maybe_add_pbft_cache(msg.uuid);
        if (!added) return;
-//        ilog("received prepare at ${n} from ${v}", ("n", msg.block_num)("v", msg.public_key));
+       ilog("received prepare at height: ${n}, view: ${v}, from ${k}, ", ("n", msg.block_num)("v", msg.view)("k", msg.public_key));
 
        pbft_controller &pcc = my_impl->chain_plug->pbft_ctrl();
        if (!pcc.pbft_db.is_valid_prepare(msg)) return;
