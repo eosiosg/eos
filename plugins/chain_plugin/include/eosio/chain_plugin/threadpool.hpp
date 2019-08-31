1735e80b917f656c3d2cc4e0a70dea9ae4efd55f
Frank-AFN <a870066721@gmail.com>
2019-08-31 12:49:27 +0800

    Add sync thread pool for chain plugin

---
+++ plugins/chain_plugin/include/eosio/chain_plugin/threadpool.hpp
@@ -0,0 +1,127 @@
+//
+// Created by Yu Yang Zhang on 8/31/19.
+//
+
+#ifndef EOSIO_THREADPOOL_HPP
+#define EOSIO_THREADPOOL_HPP
+
+#include <thread>
+#include <memory>
+#include <list>
+#include <iostream>
+#include <condition_variable>
+#include <mutex>
+#include <memory>
+#include <boost/thread/thread.hpp>
+#include <boost/function.hpp>
+using namespace std;
+
+namespace eosio{
+
+typedef boost::function<void()> func_type;
+
+class thread_wrapper {
+private:
+	unique_ptr<std::thread> thread;
+	std::condition_variable cv_wait_task;
+	std::mutex mutex_cv_wait_task;
+	bool task_ready = false;
+	bool finish = false;
+	bool stop = false;
+	std::mutex mutex_stop;
+	func_type func;
+	std::condition_variable cv_wait_finish;
+	std::mutex mutex_cv_wait_finish;
+
+	void thread_function(){
+		bool s;
+		{
+			std::lock_guard<std::mutex> lg(mutex_stop);
+			s = stop;
+		}
+		while(!s) {
+			{
+				std::unique_lock<std::mutex> lock(mutex_cv_wait_task);
+				cv_wait_task.wait(lock, [this] { return task_ready; });
+				{
+					std::lock_guard<std::mutex> lg(mutex_stop);
+					s = stop;
+				}
+				if(s) break;
+				task_ready = false;
+			}
+			func();
+			{
+				std::unique_lock<std::mutex> lock(mutex_cv_wait_finish);
+				finish = true;
+				cv_wait_finish.notify_one();
+			}
+		}
+	}
+public:
+	thread_wrapper() {
+		thread = make_unique<std::thread>(&thread_wrapper::thread_function, this);
+	}
+	void set_run_func(func_type f){
+		func = f;
+	}
+	void run() {
+		std::unique_lock<std::mutex> lock(mutex_cv_wait_task);
+		task_ready = true;
+		cv_wait_task.notify_one();
+	}
+	void wait() {
+		std::unique_lock<std::mutex> lock(mutex_cv_wait_finish);
+		cv_wait_finish.wait(lock, [this]{return finish;});
+		finish = false;
+	}
+
+	~thread_wrapper() {
+		{
+			std::lock_guard<std::mutex> lg(mutex_stop);
+			stop = true;
+		}
+		{
+			std::unique_lock<std::mutex> lock(mutex_cv_wait_task);
+			task_ready = true;
+			cv_wait_task.notify_one();
+		}
+		thread->join();
+	}
+};
+
+
+
+class thread_pool {
+private:
+	std::list<unique_ptr<thread_wrapper>> threads;
+	std::mutex mutex;
+public:
+	thread_pool(int n){
+		for(auto i = 0; i < n; i++){
+			threads.emplace_back(make_unique<thread_wrapper>());
+		}
+	}
+	~thread_pool() {
+		while(not threads.empty()){
+			threads.pop_front();
+		}
+	}
+	unique_ptr<thread_wrapper> get_thread() {
+		std::lock_guard<std::mutex> lg(mutex);
+		if(!threads.empty()) {
+			auto p =  std::move( threads.front());
+			threads.pop_front();
+			return p;
+		}
+		return nullptr;
+	}
+
+	void return_thread(unique_ptr<thread_wrapper>&& tw) {
+		std::lock_guard<std::mutex> lg(mutex);
+		threads.emplace_back(std::move(tw));
+	}
+};
+}
+
+#endif //EOSIO_THREADPOOL_HPP
